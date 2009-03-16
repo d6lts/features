e85c7c524eb35267ffba495dc673acbd32bed55b
young hahn <yhahn@264833.no-reply.drupal.org>
2009-03-16 23:26:54 +0000

    - JS: Adding check before attempting to attach behaviors to module name input. - Adding sort by name to module query. - Fixing feature form styling to have less extreme metrics.

--- features.js
+++ features.js
@@ -12,25 +12,27 @@ Drupal.behaviors.featuresTable = function() {
 };
 
 Drupal.behaviors.featuresMachineReadable = function() {
-  $('.feature-name').after(' <small class="feature-filename-suffix">&nbsp;</small>');
-  if ($('.feature-filename').val() == $('.feature-name').val().toLowerCase().replace(/[^a-z0-9]+/g, '_').replace(/_+/g, '_') || $('.feature-filename').val() == '') {
-    $('.feature-filename').parents('.form-item').hide();
-    $('.feature-name').keyup(function() {
-      var machine = $(this).val().toLowerCase().replace(/[^a-z0-9]+/g, '_').replace(/_+/g, '_');
-      if (machine != '_' && machine != '') {
-        $('.feature-filename').val(machine);
-        $('.feature-filename-suffix').empty().append(' Machine name: ' + machine + ' [').append($('<a href="#">'+ Drupal.t('Edit') +'</a>').click(function() {
-          $('.feature-filename').parents('.form-item').show();
-          $('.feature-filename-suffix').hide();
-          $('.feature-name').unbind('keyup');
-          return false;
-        })).append(']');
-      }
-      else {
-        $('.feature-filename').val(machine);
-        $('.feature-filename-suffix').text('');
-      }
-    });
-    $('.feature-name').keyup();
+  if ($('.feature-name').size() > 0) {
+    $('.feature-name').after(' <small class="feature-filename-suffix">&nbsp;</small>');
+    if ($('.feature-filename').val() == $('.feature-name').val().toLowerCase().replace(/[^a-z0-9]+/g, '_').replace(/_+/g, '_') || $('.feature-filename').val() == '') {
+      $('.feature-filename').parents('.form-item').hide();
+      $('.feature-name').keyup(function() {
+        var machine = $(this).val().toLowerCase().replace(/[^a-z0-9]+/g, '_').replace(/_+/g, '_');
+        if (machine != '_' && machine != '') {
+          $('.feature-filename').val(machine);
+          $('.feature-filename-suffix').empty().append(' Machine name: ' + machine + ' [').append($('<a href="#">'+ Drupal.t('Edit') +'</a>').click(function() {
+            $('.feature-filename').parents('.form-item').show();
+            $('.feature-filename-suffix').hide();
+            $('.feature-name').unbind('keyup');
+            return false;
+          })).append(']');
+        }
+        else {
+          $('.feature-filename').val(machine);
+          $('.feature-filename-suffix').text('');
+        }
+      });
+      $('.feature-name').keyup();
+    }
   }
 }
