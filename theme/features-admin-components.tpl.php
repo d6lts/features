9aaa811cb418b3f578711a11ceae0d3147bb516d
young hahn <yhahn@264833.no-reply.drupal.org>
2010-06-30 22:57:47 +0000

    #577674 by dmitrig01: Display reasons for Incompatible Feature.

--- theme/features-admin-components.tpl.php
+++ theme/features-admin-components.tpl.php
@@ -12,7 +12,12 @@
   <div class='column'>
     <div class='components'>
       <?php print $components ?>
-      <div class='buttons clear-block'><?php print $buttons ?></div>
+      <?php if (!empty($key)): ?>
+        <div class='clear-block features-key'><?php print theme('links', $key) ?></div>
+      <?php endif; ?>
+      <?php if (!empty($buttons)): ?>
+        <div class='buttons clear-block'><?php print $buttons ?></div>
+      <?php endif; ?>
     </div>
   </div>
   <?php print $form ?>
