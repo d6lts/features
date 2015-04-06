d0085ce5cb4e3761d18905a7bc096e6f62afae88
Mike Potter <mpotter@phase2technology.com>
2015-04-06 17:08:13 -0400

    Update ConfigInstaller for latest D8 beta changes

--- src/FeaturesConfigInstaller.php
+++ src/FeaturesConfigInstaller.php
@@ -16,12 +16,31 @@ class FeaturesConfigInstaller extends ConfigInstaller {
   /**
    * {@inheritdoc}
    */
-  public function findPreExistingConfiguration($type, $name) {
+  protected function findPreExistingConfiguration(StorageInterface $storage) {
+    // Override Drupal\Core\Config\ConfigInstaller::findPreExistingConfiguration
+    // Allow config that already exists coming from Features
     $manager = \Drupal::service('features.manager');
-    if (($type == 'module') && $manager->isFeatureModule($name)) {
-      // Allow a Features package to be enabled even if it's config conflicts.
-      return array();
+    $assigner = \Drupal::service('features_assigner');
+    $assigner->assignConfigPackages();
+    $features_data = $manager->getConfigCollection();
+
+    $existing_configuration = array();
+    // Gather information about all the supported collections.
+    $collection_info = $this->configManager->getConfigCollectionInfo();
+
+    foreach ($collection_info->getCollectionNames() as $collection) {
+      $config_to_create = array_keys($this->getConfigToCreate($storage, $collection));
+      $active_storage = $this->getActiveStorages($collection);
+      foreach ($config_to_create as $config_name) {
+        if ($active_storage->exists($config_name)) {
+          // Test if config is part of a Feature package.
+          if (!isset($features_data[$config_name]) || empty($features_data[$config_name]['package'])) {
+            $existing_configuration[$collection][] = $config_name;
+          }
+        }
+      }
     }
-    return parent::findPreExistingConfiguration($type, $name);
+    return $existing_configuration;
   }
+
 }
